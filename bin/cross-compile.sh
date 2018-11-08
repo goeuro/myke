9753bfc53160da905b183bd8551c993cbeb9adb4
Subhas Dandapani <rdsubhas@gmail.com>
2018-11-08 03:36:04 +0100

    Update licenses

--- bin/cross-compile.sh
+++ bin/cross-compile.sh
@@ -11,20 +11,21 @@ version=${1?"version is required"}
 echo $version >> "tmp/version"
 
 # Generate license notices
+go mod vendor
 deps="github.com/goeuro/myke $(go list -f '{{ join .Deps "\n"}}' ./... | grep -v 'goeuro/myke')"
 out="tmp/LICENSES"
 echo -e "OPEN SOURCE LICENSES\n" > $out
 
 for dep in $deps; do
-	if [ -d "$GOPATH/src/$dep" ]; then
-		notices=$(ls -d $GOPATH/src/$dep/* 2>/dev/null | grep -i -e "license" -e "licence" -e "copying" -e "notice" || echo)
+	if [ -d "vendor/$dep" ]; then
+		notices=$(ls -d vendor/$dep/* 2>/dev/null | grep -i -e "license" -e "licence" -e "copying" -e "notice" || echo)
 		if [ ! -z "$notices" ]; then
-			echo -e "$dep\n\n" >> $out
+			echo -e "BEGIN LICENSE FOR $dep\n\n" >> $out
 			for notice in $notices; do
 				echo "Adding license: $notice"
 				cat $notice >> $out
 			done
-			echo -e "\n\n" >> $out
+			echo -e "\nEND LICENSE FOR $dep\n\n" >> $out
 		fi
 	fi
 done
